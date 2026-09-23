5ce6183808b1c4466b348ff914dee214caba2050
Naeun-K <nek7148@gmail.com>
2026-09-23 17:04:29 +0900

    feat:프로젝트 추가

---
+++ app.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   document
+//     .querySelectorAll(".section-profile-typing-container > strong")
+//     .forEach(function (el) {
+//       // trigger animation by adding the active class
+//       void el.offsetWidth;
+//       el.classList.add("typing-active");
+//     });
+// });
